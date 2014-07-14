593eaf72dc1c55bdfd012666a43dbb5fd3b40be0
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2014-07-14 14:19:26 +0900

    tmp

--- systemValiables.h
+++
@@ -1,6 +0,0 @@
-#include <GL/glut.h>
-
-unsigned char	mouseFlag = GL_FALSE;		// flag for moving or not
-int				xStart, yStart;				// start position when drug begins
-double			xAngle = 0.0, yAngle = 0.0;	// angles of the teapot
-int samplingTime = 50;
